cc992386a6a5a63e1231fbe80a3e279eff9dae57
someone <someone@someplace.com>
2025-02-22 16:45:47 +0100

    Q2b added, but not correct. 2a corrected, but still probably incorrect.

--- 2_vandermonde_matrix.py
+++ 2_vandermonde_matrix.py
@@ -8,8 +8,7 @@ import os
 import matplotlib.pyplot as plt
 
 
-
-# TODO overflow fixen
+# gecheckt, werkt voor A, geeft geen nans meer voor V
 def LU_decomposition(matrix:np.ndarray):
     # Assume alpha_ii = 1
     # Loop over the columns j
@@ -22,14 +21,17 @@ def LU_decomposition(matrix:np.ndarray):
                 term += matrix[i,k]*matrix[k,j]
             matrix[i,j] -= term
         # Update alpha_ij where i>j
-        for i in range(j, matrix.shape[0]):
+        beta_jj = 1/matrix[j,j]
+        for i in range(j+1, matrix.shape[0]):
             term = np.float64(0)
             for k in range(0, j):
                 term += matrix[i,k]*matrix[k,j]
-            matrix[i,j] = (matrix[i,j]-term)/matrix[j,j]
-    return matrix
+            matrix[i,j] = (matrix[i,j]-term)*beta_jj
+    return np.float64(matrix)
+
 
 def solving_system_with_LU(LU:np.ndarray, b:np.array) -> np.array:
+    # Knowing that alpha_ii = 1
     # Forward substitution
     # Keep y_0 = b_0 as alpha_00 = 1
     for i in range(1, LU.shape[0]):
@@ -42,26 +44,64 @@ def solving_system_with_LU(LU:np.ndarray, b:np.array) -> np.array:
     for i in range(LU.shape[0]-1):
         i = LU.shape[0]-1 - i
         term = np.float64(0)
-        for j in range(i):
+        for j in range(i+1, LU.shape[0]):
             term += LU[i,j]*b[j]
         b[i] = (b[i]-term)/LU[i,i]
-    return b
+    return np.float64(b)
 
 
 # TODO Neville's algorithm
 
+def bisection(M:np.int64, x_samp:np.array, x_interp:np.array) -> np.ndarray:
+# Assumes x_samp are uniformly spaced
+    if (M > x_samp.shape[0]):
+        print("M is too large for the array x_samp")
+        return False
+    for (i,x) in enumerate(x_interp):
+        l_idx = np.int64(0)
+        r_idx = np.int64(x_samp.shape[0]-1)
+        while (r_idx - l_idx > 1):
+            m_idx = np.int64((l_idx + r_idx)*0.5)
+            if (x <= x_samp[m_idx]):
+                r_idx = m_idx
+            elif (x > x_samp[m_idx]):
+                l_idx = m_idx
+        # Computing the left boundary index (ignoring range)
+        l_idx -= np.int64(np.floor(0.5*M))
+        # Checking range
+        if l_idx < 0:
+            l_idx = 0
+        if (l_idx + M >= x_samp.shape[0]):
+            l_idx -= l_idx + M - x_samp.shape[0]
+        x_interp[i] = l_idx
+    return np.int64(x_interp)
 
+def neville(M, x_samp, y_samp, x_interp):
+    # Finding left index of M tabulated points using bisection
+    l_indices = bisection(M, x_samp.copy(), x_interp.copy())
+    #y_samp = y_samp[np.newaxis,:]
+    # Initializing matrix P
+    Ps = np.zeros((x_interp.shape[0], M), dtype=np.float64)
+    for i in range(M):
+        Ps[:,i] = y_samp[l_indices+i]
+    print(Ps)
+    # Looping over orders k and current intervals
+    for k in range(1, M):
+        for i in range(M-1-k):
+            j = i+k
+            Ps[:,i] = ((x_samp[l_indices+j]-x_interp)*Ps[:,i]+(x_interp-x_samp[l_indices+i])*Ps[:,j])/(x_samp[l_indices+j]-x_samp[l_indices+i])
+    error_estimate = np.absolute(Ps[:,0]-Ps[:,1])
+    return Ps[:,0], error_estimate
 
-
-
-
+# TODO something wrong with neville (bisection seems fine)
+# For lower M, result is better than for larger M (shouldnt be right)
 
 data=np.genfromtxt(os.path.join(sys.path[0],"Vandermonde.txt"),comments='#',dtype=np.float64)
 x=data[:,0]
 y=data[:,1]
 xx=np.linspace(x[0],x[-1],1001) #x values to interpolate at
 
-
+#Insert your own code to calculate interpolated y values here!
 
 # 2a
 
@@ -69,24 +109,31 @@ xx=np.linspace(x[0],x[-1],1001) #x values to interpolate at
 V = np.zeros((x.shape[0], x.shape[0]), dtype=np.float64)
 for j in range(V.shape[0]):
     V[:,j] = np.power(x,j)
-#print(V)
+#print(V) this is correct TODO
+
+# check, LU is correct
+# A = np.array([[1,2,4],[3,8,14],[2,6,13]])
+# print(A)
+# print(LU_decomposition(A))
 
 # LU-decomposing V
 LU_V = LU_decomposition(V.copy())
-print(LU_V)
+#print(LU_V)
 
 # Solving for c
 c = solving_system_with_LU(LU_V, y.copy())
-print(c)
+#print(c)
 
 # Constructing the polynomial y
 def polynomial(c, x):
     y = np.float64(0)
     for j in range(c.shape[0]):
         y += c[j] * np.power(x,j)
-    return y
+    return np.float64(y)
 
-#Insert your own code to calculate interpolated y values here!
+# 2b
+
+# 3b
 
 
 #The plotting code below assumes you've given the interpolated
@@ -99,8 +146,8 @@ def polynomial(c, x):
 # ya=np.zeros(len(x),dtype=np.float64) #replace!
 yya = polynomial(c, xx)
 ya = polynomial(c, x)
-yyb=yya #replace!
-yb=ya #replace!
+yyb = neville(3, x, y, xx)[0]
+yb = neville(3, x, y, x)[0]
 yyc1=yya #replace!
 yc1=ya #replace!
 yyc10=yya #replace!
