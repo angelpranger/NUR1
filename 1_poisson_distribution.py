42d1669c5db0d30f23be784843f2f6c09c00a097
someone <someone@someplace.com>
2025-02-26 10:25:53 +0100

    test voor vdesk

--- 1_poisson_distribution.py
+++ 1_poisson_distribution.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-
 import numpy as np
 
-# overflow caused by the factorial in the traditional way for large k
-
-# using log conversion
+# Using log conversion
 def poisson1(lamda:np.float32, k:np.float32) -> np.float32:
+    """Computing the poisson distribution value for parameters (lamda, k) using log conversion."""
     j = np.float32(0)
     for i in range(np.int32(k)):
         i = np.float32(i)
@@ -13,16 +11,18 @@ def poisson1(lamda:np.float32, k:np.float32) -> np.float32:
     P = np.float32(np.exp(k*np.log(lamda)-lamda-j))
     return P
 
-# using different order of operations
+# Using different order of operations
 def poisson2(lamda:np.float32, k:np.float32) -> np.float32:
+    """Computing the poisson distribution value for parameters (lamda, k) using an unconventional order of operations."""
     P = np.float32(np.exp(-lamda))
     for i in range(np.int32(k)):
         i = np.float32(i)
         P *= np.float32(lamda/(k-i))
     return P
 
-# using different order of operations
+# Using different order of operations and log conversion, avoiding np.exp(-lamda)
 def poisson3(lamda:np.float32, k:np.float32) -> np.float32:
+    """Computing the poisson distribution value for parameters (lamda, k) using a different order of operations and a log conversion."""
     P = np.float32(1)
     for i in range(np.int32(k)):
         i = np.float32(i)
@@ -30,13 +30,11 @@ def poisson3(lamda:np.float32, k:np.float32) -> np.float32:
     P = np.float32(np.exp(np.log(P) - lamda))
     return P
 
+# Computing and printing results
 lamda_k = np.array([[1,0],[5,10],[3,21],[2.6,40],[100,5],[101,200]], dtype=np.float32)
 print("[lamda k] P1 P2 P3")
 for values in lamda_k:
     P1 = poisson1(values[0], values[1])
     P2 = poisson2(values[0], values[1])
     P3 = poisson3(values[0], values[1])
-    print(values, P1, P2, P3)
-
-# TODO: why do the results for the last two values differ? seems like the exponent gives a large round off error
-# version 3 is the most accurate according to wolframalpha, but why? Im not sure
+    print(f"[{values[0]:.1f} {values[1]:.0f}] {P1:.6} {P2:.6} {P3:.6}")
